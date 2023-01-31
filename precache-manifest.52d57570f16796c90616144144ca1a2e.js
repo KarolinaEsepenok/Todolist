1f162497e22642618dc1f3311b8804fbb3706df8
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-01-31 15:36:39 +0300

    Updates

---
+++ precache-manifest.52d57570f16796c90616144144ca1a2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7dc64777d6b1e409a45885b19c68bda",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b3bb9fd2cdbab91df2f2",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "9cde3faecab2d0a70b89",
+    "url": "/todolist/static/js/2.cf1fcf8b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist/static/js/2.cf1fcf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3bb9fd2cdbab91df2f2",
+    "url": "/todolist/static/js/main.ed12df5c.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
