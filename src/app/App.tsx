0107e8f43ec8364113efc11285fe7aa0315119ad
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-02-02 14:02:55 +0300

    16lesson

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,70 +1,38 @@
-import React, {useCallback, useEffect} from 'react'
+import React from 'react'
 import './App.css'
-import {
-    AppBar,
-    Button,
-    CircularProgress,
-    Container,
-    IconButton,
-    LinearProgress,
-    Toolbar,
-    Typography
-} from '@material-ui/core'
+import {AppBar, Button, Container, IconButton, LinearProgress, Toolbar, Typography} from '@material-ui/core'
 import {Menu} from '@material-ui/icons'
-import {TodolistsList} from '../features/TodolistsList'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
-import {useDispatch, useSelector} from 'react-redux'
-import {appActions} from '../features/Application'
-import {Route} from 'react-router-dom'
-import {authActions, Login} from '../features/Auth'
-import {selectIsInitialized, selectStatus} from '../features/Application/selectors'
-import {authSelectors} from '../features/Auth'
-import {useActions} from '../utils/redux-utils'
+import {useSelector} from 'react-redux'
+import {AppRootStateType} from './store'
+import {RequestStatusType} from './app-reducer'
 
-type PropsType = {}
+type PropsType = {
+    demo?: boolean
+}
 
-function App(props: PropsType) {
-    const status = useSelector(selectStatus)
-    const isInitialized = useSelector(selectIsInitialized)
-    const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn)
-
-    const {logout} = useActions(authActions)
-    const {initializeApp} = useActions(appActions)
-    useEffect(() => {
-        if (!isInitialized) {
-            initializeApp()
-        }
-    }, []);
-
-    const logoutHandler = useCallback(() => {
-        logout()
-    }, [])
-    if (!isInitialized) {
-        return <div
-            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
-            <CircularProgress/>
-        </div>
-    }
+function App({demo = false}: PropsType) {
+    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
     return (
-            <div className="App">
-                <ErrorSnackbar/>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                            <Menu/>
-                        </IconButton>
-                        <Typography variant="h6">
-                            News
-                        </Typography>
-                        {isLoggedIn && <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
-                    </Toolbar>
-                    {status === 'loading' && <LinearProgress/>}
-                </AppBar>
-                <Container fixed>
-                    <Route exact path={'/'} render={() => <TodolistsList demo={false}/>}/>
-                    <Route path={'/login'} render={() => <Login/>}/>
-                </Container>
-            </div>
+        <div className="App">
+            <ErrorSnackbar />
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+             { status === 'loading' &&  <LinearProgress /> }
+            </AppBar>
+            <Container fixed>
+                <TodolistsList demo={demo}/>
+            </Container>
+        </div>
     )
 }
 
